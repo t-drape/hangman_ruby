238730b961b09e0e4474d315c3a592ea75d88e79
t-drape <thebatreview1@gmail.com>
2024-08-19 17:02:57 -0700

    Add all functionality for gameplay, Add messages as well

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,10 +3,58 @@ def get_random_word(lines)
   acceptable_lines.sample
 end
 
+def display_letters(random_word)
+  guessed_letters = ""
+  random_word.length.times {guessed_letters += "_ "}
+  guessed_letters
+end
+
+def user_guess(alphabets)
+  puts "What's your letter guess?"
+  letter_guess = gets.chomp.downcase
+  letter_guess = user_guess(alphabets) unless alphabets.include?(letter_guess)
+  letter_guess
+
+end
+
+
 def play_game
+
   lines = File.readlines('google-10000-english.txt')
-  random_word = get_random_word(lines)
-  puts random_word
+  lines = lines.map {|line| line.chomp}
+  random_word_array = get_random_word(lines).split("")
+  num_guesses = 0
+  alphabets = [*"a".."z"]
+
+  game_won = false
+  guess_right = false
+  counter = 0
+  random_word_array = get_random_word(lines).split("")
+  guessed_letters = display_letters(random_word_array).split
+  until num_guesses == 7 || game_won == true
+    guess_right = false
+    letter_guess = user_guess(alphabets)
+    if random_word_array.include?(letter_guess)
+      guess_right = true
+      random_word_array.each_with_index do |letter, index|
+        guessed_letters[index] = letter if letter == letter_guess
+        counter += 1 if letter == letter_guess
+      end
+    end
+    alphabets = alphabets.select {|l| l != letter_guess}
+    if guess_right == true
+      if counter == random_word_array.length
+        game_won = true
+      end
+    else
+      4.times do 
+        puts "      |"
+        sleep 0.5
+      end
+      num_guesses += 1
+    end
+    p guessed_letters
+  end
 end
 
 play_game
